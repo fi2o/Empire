7eba1c20d3aaf4389e07adec207872b73d9ad560
Brian <brian@briangp.com>
2014-02-24 14:54:04 -0500

    initial commit

---
+++ templates/foot.php
@@ -0,0 +1,9 @@
+
+
+			</div> <!-- close .body -->
+			<div class="footer">
+				
+			</div>
+		</div> <!-- close .container -->
+	</body>
+</html>
---
+++ templates/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	
+		<meta name="HandheldFriendly" content="true" />
+		
+		<title>Empire Management CT</title>
+		
+		<link rel="stylesheet" type="text/css" src="../styles/style.css" />
+		<script type='text/javascript' src='http://code.jquery.com/jquery-1.9.0.min.js'></script>
+		
+	</head>
+	<body>
+		
+		<div class="container">
+			<div class="header">
+				
+			</div>
+			<div class="body">
+				
